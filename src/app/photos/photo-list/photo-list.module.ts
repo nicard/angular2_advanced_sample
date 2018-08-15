16b151e8817cae0a6775302e2700cbd7bafe6e89
Richard <rra_ncd@hotmail.com>
2018-08-15 20:38:17 -0300

    created a first event to search component interact with photo-list component

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,13 +6,15 @@ import {FilterByDescriptionPipe} from './filter-by-description.pipe';
 import {CommonModule} from '@angular/common';
 import {PhotoModule} from '../photo/photo.module';
 import {CardModule} from '../../shared/card/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     PhotoListComponent,
     LoadButtonComponent,
     PhotosComponent,
-    FilterByDescriptionPipe
+    FilterByDescriptionPipe,
+    SearchComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit, OnDestroy {
+  @Output() onTyping = new EventEmitter<string>();
+  @Input() value = '';
+  debounce: Subject<string> = new Subject<string>();
+  constructor() { }
+
+  ngOnInit() {
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.onTyping.emit(filter));
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+}
