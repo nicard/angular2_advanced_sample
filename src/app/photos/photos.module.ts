3313ba652482ab51bae5cce2b8f70573c9bb9478
Richard <rra_ncd@hotmail.com>
2018-08-15 19:30:40 -0300

    Organizated my module and create more sub-modules.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,28 +2,20 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {CommonModule} from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import {FilterByDescriptionPipe} from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import {PhotoModule} from './photo/photo.module';
+import {PhotoListModule} from './photo-list/photo-list.module';
+import {PhotoFormModule} from './photo-form/photo-form.module';
 
 @NgModule({
   declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescriptionPipe,
-    LoadButtonComponent
+
   ],
   imports: [
     CommonModule,
-    HttpClientModule
-  ],
-  exports: [
-    PhotoListComponent
+    HttpClientModule,
+    PhotoModule,
+    PhotoListModule,
+    PhotoFormModule
   ]
 })
 export class PhotosModule { }
