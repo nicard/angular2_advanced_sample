cdcb3598c4d73b4e1facbd10503662a0ba2f7c49
Richard <rra_ncd@hotmail.com>
2018-08-15 19:17:12 -0300

    create a new component to load and paginate our photos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -3,6 +3,7 @@ import {Photo} from '../photo/photo';
 import {ActivatedRoute} from '@angular/router';
 import {Subject} from 'rxjs';
 import {debounceTime} from 'rxjs/operators';
+import {PhotoService} from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -14,9 +15,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter = '';
   debounce: Subject<string> = new Subject<string>();
-  constructor(private activatedRoute: ActivatedRoute) { }
+  hasMore = true;
+  currentPage = 1;
+  userName = '';
+  constructor(private activatedRoute: ActivatedRoute, private service: PhotoService) { }
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounce
       .pipe(debounceTime(300))
@@ -25,6 +30,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.service.listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
   }
 
 }
